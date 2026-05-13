7c1efa71e0c6309711f61e35777c71e7f3366e30
Siddhesh Asati <130655179+siddheshasati@users.noreply.github.com>
2026-05-13 16:13:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import json, os
 
 def fetch_poster(movies_id):
     response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movies_id))
