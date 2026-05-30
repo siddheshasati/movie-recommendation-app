5a9237a11ea6a01bcf8b69b23c9b8b7d9ac355b9
Siddhesh Asati <130655179+siddheshasati@users.noreply.github.com>
2026-05-30 15:29:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import requests
 import json, os
 
+#movie poster though APIs
 def fetch_poster(movies_id):
     response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movies_id))
     data=response.json()
